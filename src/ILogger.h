e68c3a8fe0e4c38d9c2945690ce172ab524cc961
Junaij M <junaijm@gmail.com>
2024-11-04 09:30:09 +0530

    Issues found in self review resolved

--- src/ILogger.h
+++ src/ILogger.h
@@ -5,6 +5,7 @@
 class ILogger
 {
 public:
+    virtual ~ILogger() = default;
     virtual void LogInfo(const std::string& logText) = 0;
     virtual void LogWarning(const std::string& logText) = 0;
     virtual void LogError(const std::string& logText) = 0;
